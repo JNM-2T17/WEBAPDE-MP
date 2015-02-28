a9be33ad3cb8775f1b94c46525f723a707c5ff6d
Austin Fernandez <ryanaustinf@yahoo.com>
2015-02-28 20:00:33 +0800

    Added Functionality to Index

---
+++ The Multimedia Terminal/WebContent/tmi.js
@@ -0,0 +1,9 @@
+function switchClass(a, b, c) {
+	a.addClass(b);
+	a.removeClass(c);
+}
+
+function setFilterCrit( a, b ){
+	switchClass( a, b == true ? 'filterCritActive' : 'filterCritInactive',
+				b == false ? 'filterCritActive' : 'filterCritInactive')
+}
