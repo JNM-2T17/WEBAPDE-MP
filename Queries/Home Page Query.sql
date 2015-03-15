8b3690a5c9820b4c9fa7526483087661c0246632
Austin Fernandez <ryanaustinf@yahoo.com>
2015-03-15 16:43:21 +0800

    Added Special Queries

---
+++ Queries/Home Page Query.sql
@@ -0,0 +1,18 @@
+SELECT W.title, WC2.name AS `Created By`, class, releaseYear, viewCount, network, studio, 
+	   publisher, wordcount, datePremiered, GN.illustrator AS `Graphic Novel Illustrator`, 
+       CM.illustrator AS `Comic/Manga Illustrator`, URL, org, 
+       album, recordingStudio 
+FROM work W 
+	 LEFT JOIN liveactionanimation LAA ON W.title = LAA.title 
+	 LEFT JOIN television T ON W.title = T.title 
+     LEFT JOIN animation A ON T.title = A.title 
+     LEFT JOIN book B ON W.title = B.title 
+     LEFT JOIN graphicnovel GN ON B.title = GN.title 
+     LEFT JOIN comicsmanga CM ON B.title = CM.title 
+     LEFT JOIN webcontent WC ON W.title = WC.title 
+     LEFT JOIN theater TH ON W.title = TH.title 
+     LEFT JOIN videogame V ON W.title = V.title 
+     LEFT JOIN music M ON W.title = M.title 
+     LEFT JOIN album AL ON W.title = AL.title, workCreator WC2 
+WHERE W.title = WC2.title AND WC2.isMain = TRUE 
+ORDER BY viewCount DESC 
