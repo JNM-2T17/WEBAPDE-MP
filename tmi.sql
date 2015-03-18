7d23daf1a706c66e2718fb24acc85e3ae5814820
Austin Fernandez <ryanaustinf@yahoo.com>
2015-03-18 09:55:42 +0800

    Updated Model

--- tmi.sql
+++ tmi.sql
@@ -442,6 +442,68 @@ CREATE TABLE IF NOT EXISTS consolegame (
 		ON DELETE CASCADE
 )engine = innoDB;
 
+CREATE VIEW `Proposals` AS
+SELECT W.title, WC2.name AS `Created By`, description, class, releaseYear, viewCount, network, studio, 
+	   publisher, wordcount, datePremiered, GN.illustrator AS `Graphic Novel Illustrator`, 
+       CM.illustrator AS `Comic/Manga Illustrator`, URL, org, 
+       album, recordingStudio 
+FROM work W 
+	 LEFT JOIN liveactionanimation LAA ON W.title = LAA.title 
+	 LEFT JOIN television T ON W.title = T.title 
+     LEFT JOIN animation A ON T.title = A.title 
+     LEFT JOIN book B ON W.title = B.title 
+     LEFT JOIN graphicnovel GN ON B.title = GN.title 
+     LEFT JOIN comicsmanga CM ON B.title = CM.title 
+     LEFT JOIN webcontent WC ON W.title = WC.title 
+     LEFT JOIN theater TH ON W.title = TH.title 
+     LEFT JOIN videogame V ON W.title = V.title 
+     LEFT JOIN music M ON W.title = M.title 
+     LEFT JOIN album AL ON W.title = AL.title
+     LEFT JOIN workCreator WC2 ON W.title = WC2.title AND WC2.isMain = TRUE
+WHERE isVerified = FALSE
+ORDER BY viewCount DESC 
+
+CREATE VIEW `All Works` AS
+SELECT W.title, WC2.name AS `Created By`, description, class, releaseYear, viewCount, network, studio, 
+	   publisher, wordcount, datePremiered, GN.illustrator AS `Graphic Novel Illustrator`, 
+       CM.illustrator AS `Comic/Manga Illustrator`, URL, org, 
+       album, recordingStudio 
+FROM work W 
+	 LEFT JOIN liveactionanimation LAA ON W.title = LAA.title 
+	 LEFT JOIN television T ON W.title = T.title 
+     LEFT JOIN animation A ON T.title = A.title 
+     LEFT JOIN book B ON W.title = B.title 
+     LEFT JOIN graphicnovel GN ON B.title = GN.title 
+     LEFT JOIN comicsmanga CM ON B.title = CM.title 
+     LEFT JOIN webcontent WC ON W.title = WC.title 
+     LEFT JOIN theater TH ON W.title = TH.title 
+     LEFT JOIN videogame V ON W.title = V.title 
+     LEFT JOIN music M ON W.title = M.title 
+     LEFT JOIN album AL ON W.title = AL.title
+     LEFT JOIN workCreator WC2 ON W.title = WC2.title AND WC2.isMain = TRUE
+ORDER BY viewCount DESC 
+
+CREATE VIEW `Home Page` AS
+SELECT W.title, WC2.name AS `Created By`, description, class, releaseYear, viewCount, network, studio, 
+	   publisher, wordcount, datePremiered, GN.illustrator AS `Graphic Novel Illustrator`, 
+       CM.illustrator AS `Comic/Manga Illustrator`, URL, org, 
+       album, recordingStudio 
+FROM work W 
+	 LEFT JOIN liveactionanimation LAA ON W.title = LAA.title 
+	 LEFT JOIN television T ON W.title = T.title 
+     LEFT JOIN animation A ON T.title = A.title 
+     LEFT JOIN book B ON W.title = B.title 
+     LEFT JOIN graphicnovel GN ON B.title = GN.title 
+     LEFT JOIN comicsmanga CM ON B.title = CM.title 
+     LEFT JOIN webcontent WC ON W.title = WC.title 
+     LEFT JOIN theater TH ON W.title = TH.title 
+     LEFT JOIN videogame V ON W.title = V.title 
+     LEFT JOIN music M ON W.title = M.title 
+     LEFT JOIN album AL ON W.title = AL.title
+     LEFT JOIN workCreator WC2 ON W.title = WC2.title AND WC2.isMain = TRUE
+WHERE isVerified = TRUE
+ORDER BY viewCount DESC 
+
 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
 SET SQL_MODE = @OLD_SQL_MODE;
